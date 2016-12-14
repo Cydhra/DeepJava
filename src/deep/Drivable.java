498ba3dd063fe516d967bc4cb604ab8923138297
Cydhra <j-hengstler@web.de>
2016-12-14 20:43:21 +0100

    Attempted to fix calls for constructors... Still problems with variadics

--- src/deep/Drivable.java
+++ src/deep/Drivable.java
@@ -6,22 +6,23 @@ import java.lang.reflect.Constructor;
  *
  */
 public interface Drivable {
-
+    
     /**
      * Truck engine enumeration
      */
     enum Engine {
         OTTO(Vendor.BENZ), DIESEL(Vendor.BMW), E(Vendor.TESLA), RASPBERRY_JUICE(Vendor.FLIWATUET);
-
+        
         enum Vendor {
             BENZ, VW, BMW, TESLA, FLIWATUET;
-
-            class Waypoint<T extends Number> {
-
+            
+            static class Waypoint<T extends Number> {
+                
                 private volatile T[] coords;
-
+                
                 /**
                  * A n dimensional waypoint of bounded generic type
+                 *
                  * @param coords
                  */
                 public Waypoint(final T... coords) {
@@ -29,19 +30,19 @@ public interface Drivable {
                 }
             }
         }
-
+        
         private final Vendor vendor;
-
+        
         Engine(final Vendor vendor) {
             this.vendor = vendor;
         }
     }
-
+    
     /**
      * Don't drink and drive!
      */
     <T extends Number> void drive(Engine.Vendor.Waypoint<T>... waypoints);
-
+    
     /**
      *
      */
@@ -50,31 +51,33 @@ public interface Drivable {
         public <T extends Number> void drive(final Engine.Vendor.Waypoint<T>... waypoints) {
             System.out.println(this.getClass().getName());
             class Truck extends Car {
-
+                
                 private final Engine engine;
-
+                
                 public Truck(Engine engine) {
                     this.engine = engine;
                 }
             }
         }
     }
-
+    
     static abstract class Bike implements Drivable {
         public void test() {
             try {
-                Class<?> truckClass = Class.forName("Drivable$Car$1Truck");
-                Constructor<?> constructor = truckClass.getConstructor(Engine.class);
+                Class<?> truckClass = Class.forName("deep.Drivable$Car$1Truck");
+                Constructor<?> constructor = truckClass.getConstructor(Car.class, Engine.class);
                 constructor.setAccessible(true);
-                Object truck = constructor.newInstance(Engine.E);
-                truckClass.getMethod("drive").invoke(truck);
+                Object truck = constructor.newInstance(new Car(), Engine.E);
+                truckClass.getMethod("drive", Engine.Vendor.Waypoint[].class)
+                        .invoke(truck, new Engine.Vendor.Waypoint[]{ new Engine.Vendor.Waypoint<Integer>() });
             } catch (Throwable e) {
                 e.printStackTrace();
             }
         }
-
+        
         /**
          * Main Entry Point
+         *
          * @param args
          */
         public static void main(String[] args) {
@@ -86,6 +89,6 @@ public interface Drivable {
             }.test();
         }
     }
-
+    
 }
 
