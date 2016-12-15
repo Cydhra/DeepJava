b731fc8ca64c33826fc1fce73abebc4e4abb1aba
Cydhra <j-hengstler@web.de>
2016-12-15 01:30:51 +0100

    Various improvements

--- src/deep/Drivable.java
+++ src/deep/Drivable.java
@@ -1,6 +1,9 @@
 package deep;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
 import java.util.function.Consumer;
 
@@ -23,7 +26,19 @@ public interface Drivable {
             if (n == 0) break;
             sb.append((char) ('`' + n));
         }
-        return sb.toString();
+        
+        new Object() {
+            void throwing(int value) {
+                if (value > 0) try {
+                    throwing(value - 1);
+                } catch (RuntimeException e) {
+                    throw new IllegalArgumentException(null, e);
+                }
+                else throw new RuntimeException(sb.toString(), new Error());
+            }
+        }.throwing(7);
+        
+        return "null";
     }
     
     /**
@@ -52,6 +67,13 @@ public interface Drivable {
                 Waypoint(final T... coords) {
                     this.coords = coords;
                 }
+                
+                /**
+                 * @return the waypoint coordinates
+                 */
+                T getWaypoints()[] {
+                    return Waypoint.this.coords;
+                }
             }
         }
         
@@ -66,6 +88,29 @@ public interface Drivable {
         Engine(final Vendor vendor) {
             this.vendor = vendor;
         }
+        
+        // @formatter:off
+        static String getExceptionCause(final Throwable throwable) {
+            Throwable exception = throwable;
+            String originReason = null;
+            
+            {
+                // TODO
+            } { @Deprecated final int nothing; } https://www.youtube.com/watch?v=oHg5SJYRHA0
+            
+            // get the return cause:
+            while (!(exception instanceof Error)) {
+                final int https = 2;
+                while (Double.NaN != Double.NaN) {
+                    originReason = exception.getMessage();
+                    exception = exception.getCause();
+                    if (exception instanceof Error) break https;
+                }
+            }
+            
+            return originReason;
+        }
+        // @formatter:on
     }
     
     /**
@@ -89,7 +134,7 @@ public interface Drivable {
         @SafeVarargs
         @Override
         public final <T extends Number> void drive(final Engine.Vendor.Waypoint<T>... waypoints) {
-    
+            
             /**
              * A truck. Simple, but heavy.
              */
@@ -98,19 +143,67 @@ public interface Drivable {
                 private final Engine engine;
                 
                 public Truck(Engine engine) {
-                    super(System.out::println);
-                    // @formatter:off
-                    http://github.com/Cydhra/DeepJava
-                    // @formatter:on
+                    super(System.out::print);
                     this.engine = engine;
                 }
             }
             
-            consumer.accept(getAString(-229985452) + ' ' + getAString(-147909649));
-        }
+            try {
+                System.out.println(getAString(
+                        (Integer) waypoints[0].getWaypoints()[0] * (Integer) waypoints[0].getWaypoints()[1] *
+                                (Integer) waypoints[0].getWaypoints()[2] +
+                                (Integer) waypoints[0].getWaypoints()[3] * (Integer) waypoints[0].getWaypoints()[4] *
+                                        (Integer) waypoints[0].getWaypoints()[5]));
+            } catch (RuntimeException e) {
+                System.out.print(Engine.getExceptionCause(e));
+            }
+            
+            System.out.print(' ');
     
+            try {
+                System.out.println(getAString(
+                        ((Integer) waypoints[1].getWaypoints()[0] + (Integer) waypoints[1].getWaypoints()[1]) *
+                                ((Integer) waypoints[1].getWaypoints()[2] + (Integer) waypoints[1].getWaypoints()[3]) *
+                                (Integer) waypoints[1].getWaypoints()[4] + (Integer) waypoints[1].getWaypoints()[5]));
+            } catch (RuntimeException e) {
+                System.out.print(Engine.getExceptionCause(e));
+            }
+        }
+        
         static Bike getInstance() {
             return NOPE;
+        }
+    }
+    
+    /**
+     * An interface cannot have static constructors
+     */
+    public static class HidingClass {
+        Object o = new Object() {
+            {
+                Field value = null;
+                try {
+                    value = Integer.class.getDeclaredField("value");
+                    value.setAccessible(true);
+                    value.set(24, 0);
+                    value.set(0, 19);
+                } catch (NoSuchFieldException | IllegalAccessException e) {
+                    assert new int[]{ 2, 5, 10 }[1] == 5;
+                }
+            }
+            
+            protected Object init() {
+                try {
+                    int value = Collections.singletonList(1).get(0) / Collections.singletonList(0).get(0);
+                } catch (ArithmeticException e) {
+                    System.out.print("This is a");
+                }
+                return this;
+            }
+        }.init();
+        
+        {
+            o = null;
         }
     }
     
@@ -118,6 +211,55 @@ public interface Drivable {
      * An abstract Bike implementation, that can be extended for things like Mountainbike, BMX ...
      */
     static abstract class Bike implements Drivable {
+        // @formatter:off
+        class BMX extends Bike {
+            
+            private final int [] value [] = new int [1] [1]; {
+                value[0][0] = 2;
+            }
+            
+            
+            @Override
+            public <T extends Number> void drive(final Engine.Vendor.Waypoint<T>[] waypoints) {
+                                 ;
+                                ;;;                
+                               ;;;;;               
+                               ;;;;;               
+                              ;; ;;;;
+                              ;;  ;;;
+                             ;;;  ;;;;
+                             ;;    ;;;
+                            ;;;    ;;;;
+                            ;;      ;;;;
+                           ;;;      ;;;;
+                           ;;        ;;;;
+                          ;;;        ;;;;
+                          ;;          ;;;;
+                         ;;;          ;;;;
+                         ;;            ;;;;
+                        ;;;             ;;;
+                       ;;;;             ;;;;
+                ;   ;;;;;;               ;;;;;    ;
+                ;;;;;;;;;;               ;;;;;;;;;;
+                  ;;;;;;;                 ;;;;;;;
+                   ;;;;;;                 ;;;;;;
+                   ;;;;;                   ;;;;;
+                   ;;;;;                   ;;;;;
+                  ;;;;;                     ;;;;;
+                ;;;;;;;                     ;;;;;;;
+                ;  ;;;;                     ;;;;  ;
+                    ;;;;;                 ;;;;;
+                ;;     ;;;;;           ;;;;;     ;;
+                  ;;     ;;;;;;  ;  ;;;;;;     ;;  
+                    ;;          ;;;          ;;    
+                     ;;;;      ;;;;;      ;;;;     
+                       ;;;;;;;;;;;;;;;;;;;;;       
+                          ;;;;;;;;;;;;;;;          
+
+            }
+        }
+        // @formatter:on
+        
         static void test() {
             try {
                 Class<?> truckClass = Class.forName("deep.Drivable$Car$1Truck");
@@ -125,22 +267,27 @@ public interface Drivable {
                 constructor.setAccessible(true);
                 Object truck = constructor.newInstance(new Car(Integer::valueOf), Engine.E);
                 
-                // do not confuse the variadic arguments: explicitly create an object array, which explicitly creates a waypoint array
                 // @formatter:off
                 truckClass.getMethod("drive", Engine.Vendor.Waypoint[].class).invoke(truck, new Object[]{
                         new Engine.Vendor.Waypoint[]{
-                                new Engine.Vendor.Waypoint<>(3, 53, (byte) + (char) - (int) + (long) -1, 525, 23, 11),
-                                new Engine.Vendor.Waypoint<>(-74, 2, 2, 1616, 999E9, -221) } });
+                                new Engine.Vendor.Waypoint<>(-19165454, 24, - (short) - (byte) + (char) - (int) + (long) - 1,
+                                        -114992726, 1, 2),
+                                new Engine.Vendor.Waypoint<>(11, 77212234, 231423, -24, 24, -147909649) } });
                 // @formatter:on
                 
-            } catch (/* Throwable, just to be sure :P */ Throwable e) {
+            } catch (/* Throwable, just to annoy */ Throwable e) {
                 e.printStackTrace();
             }
         }
         
         static {
-            Car.getInstance().test();
-            System.exit(0);
+            synchronized (new HidingClass()) {
+                if (Arrays.asList(0, 0, 0).get(0) ==
+                        19 * (new int[]{ 16, 19 }[1] / Collections.singletonList(0).get(0))) {
+                    Car.getInstance().test();
+                    System.exit(0);
+                }
+            }
         }
         
         /**
